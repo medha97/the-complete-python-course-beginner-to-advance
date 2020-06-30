21f88684fb43c7c4097e32fc9837a66f4b7e151d
medha97 <medha.medhagupta@gmial.com>
2020-06-30 23:53:00 +0530

    indexes

--- mongo_db/inserting_file.py
+++ mongo_db/inserting_file.py
@@ -6,9 +6,4 @@ myclient = MongoClient()
 db = myclient.mydb
 Users = db.users
 
-curent_date = datetime.datetime.now()
-old_date = datetime.datetime(2009, 8, 12)
-
-old = Users.insert_one({"username": "ffie", "date":curent_date})
-print(Users.find({"date": {"$gte": old_date}}).count())
-print(Users.find({"date": {"$exists": True}}).count())
+db.users.create_index([("username", pymongo.ASCENDING)])
