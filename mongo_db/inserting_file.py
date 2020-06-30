de41802fecd3031068359eca592e9b919cd03e49
medha97 <medha.medhagupta@gmial.com>
2020-06-30 23:28:10 +0530

    counting document

--- mongo_db/inserting_file.py
+++ mongo_db/inserting_file.py
@@ -6,3 +6,5 @@ Users = db.users
 users = [{"username": "third","password": "12345"},{"username":"red", "password": "blue"}]
 inserted = Users.insert_many(users)
 inserted.inserted_ids
+Users.find().count()
+User.find({"fav_number": 445}).count()
