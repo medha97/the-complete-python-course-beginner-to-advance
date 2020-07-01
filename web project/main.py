634214a1196a568fecfdd97e1ee84acc1b01c62b
medha97 <medha.medhagupta@gmial.com>
2020-07-01 15:33:41 +0530

    HTML Templates

--- web project/main.py
+++ web project/main.py
@@ -1,14 +1,15 @@
 import web
 
 urls = (
-    '/(.*)','index'
+    '/(.*)/(.*)','index'
 )
 
+render = web.template.render("resources/")
 app = web.application(urls, globals())
 
 class index:
-    def GET(self, name):
-        return "<h1>hello " + name + ".</h1> How are you today?"
+    def GET(self, name, age):
+        return render.main(name, age)
 
 if __name__ =="__main__":
     app.run()
